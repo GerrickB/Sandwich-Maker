bf590e926d89fa6eaea9091c636b88d28899aa64
GerrickB <27402037+GerrickB@users.noreply.github.com>
2023-04-03 22:39:39 -0700

    Update functions to randomize ingredients

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 // Functions
 const returnRandBread = () => {
-
+  const breadChoices = ['White', 'Toast', 'Sourdough', 'Wheat', 'Garlic'];
+  return breadChoices[Math.floor(Math.random() * 5)];
 };
 
 const returnRandCheese = () => {
-
+  const cheeseChoices = ['American', 'Grilled', 'Swiss', 'Pepper', 'Mozarella'];
+  return cheeseChoices[Math.floor(Math.random() * 5)];
 };
 
 const returnRandMeat = () => {
-
+  const meatChoices = ['Bacon', 'Chicken', 'Ham', 'Roast Beef', 'Pepperoni'];
+  return meatChoices[Math.floor(Math.random() * 5)];
 };
 
 const returnRandSalad = () => {
-
+  const saladChoices = ['Lettuce', 'Tomato', 'Onions', 'Avocado', 'Kimchi'];
+  return saladChoices[Math.floor(Math.random() * 5)];
 };
 
 // Factory object
